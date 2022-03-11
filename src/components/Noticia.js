b7fe5d52d6116cc95f5b9a3a414594f6dfaad40f
rdiazca <rdiazca96@gmail.com>
2022-03-10 23:45:38 -0500

    Cargar las imágenes condicionalmente

--- src/components/Noticia.js
+++ src/components/Noticia.js
@@ -3,12 +3,19 @@ import React from 'react'
 const Noticia = ({noticia}) => {
     //extraer los datos
     const { urlToImage, url, title, description, source } = noticia;
+ 
+    //condicionalmente cargar la imagen si está disponible
+    const imagen = (urlToImage) ? 
+        <div className='card-image'>
+            <img src = {urlToImage} alt = {title} />
+            <span className='card-title'>{source.name}</span>
+        </div>
+    : null;
+
     return (
         <div className='col s12 m6 l4'>
             <div className='card'>
-                <div className='card-image'>
-                    <img src = {urlToImage} alt = {title} />
-                </div>
+                {imagen}
                 <div className='card-content'>
                     <h3>{title}</h3>
                     <p>{description}</p>
