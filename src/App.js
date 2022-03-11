27ebc3836c32e90628e8e677235c75609c28e4a6
rdiazca <rdiazca96@gmail.com>
2022-03-10 23:17:52 -0500

    Iterar las noticias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from "react";
 import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
 
 class App extends Component{
   state = { 
@@ -27,7 +28,9 @@ class App extends Component{
         titulo='Noticias React API'
         />
         <div className="container white contenedor-noticas">
-          
+            <ListaNoticias
+              noticias= {this.state.noticias}
+            />
           </div>
       </Fragment>
       );
---
+++ src/components/Noticia.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Noticia = ({noticia}) => {
+    return (
+        <h1>Test</h1>
+    );
+}  
+        
+    
+
+
+export default Noticia
